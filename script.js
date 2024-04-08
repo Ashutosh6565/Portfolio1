c3dad264584d1d7b423dc6f3cdada98756346d0a
Ashutosh <kumarashutosh7302401472@gmail.com>
2024-04-08 10:09:28 +0530

    this is where more  complicated task starts

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+const dets = {
+    clientX: 0,
+    clientY: 0
+};
+
+function circleMouseFollower(dets) {
+    window.addEventListener("mousemove", function (dets) {
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+
+circleMouseFollower(dets); // Pass the 'dets' object as an argument here
