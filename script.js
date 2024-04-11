f9b6925f25b970e5c58fe9e9894cc09e3a61c9cb
Ashutosh <kumarashutosh7302401472@gmail.com>
2024-04-11 16:12:29 +0530

    here i am using javascript to add animations to the project

--- script.js
+++ script.js
@@ -7,6 +7,33 @@ const dets = {
     clientX: 0,
     clientY: 0
 };
+function firstPageAnim(){
+    var tl =gsap.timeline();
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 2,
+        ease: Expo.easeInOut
+    })
+    .to(".boundingelem",{
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay:-1,
+        stagger: .2
+    })
+    .from("#herofooter",{
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
+    })
+}
+
+function circleChaptaKaro(){
+    window.addEventListener("")
+}
 
 function circleMouseFollower(dets) {
     window.addEventListener("mousemove", function (dets) {
@@ -14,4 +41,5 @@ function circleMouseFollower(dets) {
     })
 }
 
-circleMouseFollower(dets); // Pass the 'dets' object as an argument here
+circleMouseFollower(dets); 
+firstPageAnim();
